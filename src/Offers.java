da2224f19655414d1eab8bb4694da1fd17b7913c
suyash.yadav <suyash.yadav@bottomline.com>
2021-10-21 10:13:02 +0530

    offers added

---
+++ src/Offers.java
@@ -0,0 +1,6 @@
+public class Offers {
+    public static void main(String[] args) {
+        System.out.println("Offer 1: 10% off on item1");
+        System.out.println("Offer 1: 20% off on item4");
+    }
+}
